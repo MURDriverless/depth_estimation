9cd5c4aa5ff453d23ce4eeddaefcb4132e1ba430
kliao1 <48614736+kliao1@users.noreply.github.com>
2021-09-26 15:02:10 +1000

    Add files via upload

---
+++ CMakeFiles/object_detection_yolo.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/workspace/calibration/learnopencv/ObjectDetection-YOLO/object_detection_yolo.cpp" "/workspace/calibration/learnopencv/ObjectDetection-YOLO/build/CMakeFiles/object_detection_yolo.dir/object_detection_yolo.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/object_detection_yolo.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/object_detection_yolo.dir/object_detection_yolo.cpp.o"
+  "object_detection_yolo"
+  "object_detection_yolo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/object_detection_yolo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
